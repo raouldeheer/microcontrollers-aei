a888cb5a9a9adfcd6ca4abbaca7020a73b0f25f3
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-02-02 14:36:44 +0100

    Fixed b7

--- Week 1/Opdracht B.7a/main.c
+++ Week 1/Opdracht B.7a/main.c
@@ -18,7 +18,53 @@ void wait( int ms )
 	}
 }
 
+
+//
+// State S1
+//
+void s1(void){
+	PORTD = 0x20;
+}
+
+//
+// State S2
+//
+void s2(void){
+	PORTD = 0x40;
+}
+
+//
+// State S3
+//
+void s3(void){
+	PORTD = 0x10;
+}
+
+//
+// State Start
+//
+void start(void){
+	PORTD = 0x80;
+}
+
+//
+// State Stop
+//
+void end(void){
+	PORTD = 0x08;
+}
+
+
 typedef enum { START, S1, S2, S3, END } ENUM_STATES;
+typedef struct {
+	void (*state)(void);
+	ENUM_STATES nextState;
+} PATTERN_STRUCT;
+
+PATTERN_STRUCT pattern[5] = {
+	{start, S2}, {s1, S3}, {s2, S1}, {s3, END}, {end, START}
+};
+
 
 int main(void)
 {
@@ -27,28 +73,7 @@ int main(void)
     while(1)
     {
 		wait(500);
-		switch (state)
-		{
-			case START:
-				PORTD = 0x80;
-				state = S2;
-				break;
-			case S1:
-				PORTD = 0x20;
-				state = S3;
-				break;
-			case S2:
-				PORTD = 0x40;
-				state = S1;
-				break;
-			case S3:
-				PORTD = 0x10;
-				state = END;
-				break;
-			case END:
-				PORTD = 0x08;
-				state = START;
-				break;
-		}
+		pattern[state].state();
+		state = pattern[state].nextState;
     }
 }
