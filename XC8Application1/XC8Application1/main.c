d04917dc84811298e069a7bbedb8994d544f8aa1
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-02-02 10:50:32 +0100

    Added code

---
+++ XC8Application1/XC8Application1/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 2/2/2022 10:49:42 AM
+ *  Author: raoul & imre
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
