322adf2c77da0d30bdc6e899d9980c1c5d858750
Raoul de Heer <raouldeheer@users.noreply.github.com>
2022-03-23 13:19:59 +0100

    Added week 5

---
+++ Week 5/Week 5/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 3/23/2022 12:33:56 PM
+ *  Author: raoul
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
